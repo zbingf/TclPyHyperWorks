79c58c804939c0bccfaa3dbb35f377f00dd24a1b
zbingf <zbingf@126.com>
2020-04-11 17:21:26 +0800

    updata

---
+++ hmSources.tcl
@@ -0,0 +1,32 @@
+# hypermesh 二次开发类型
+# Tcl GUI Commands
+# Tcl Modify Commands
+# Tcl Query Commands
+# Utility Menu Commands
+
+
+
+
+# 获取 整数值 hm_getint
+# set a [hm_getint]
+# set count [hm_getint "Count=" "Please specify the count"]
+
+# 获取 浮点值 
+# set distance [hm_getfloat "Distance=" "Please specify a distance"]
+
+# 获取字符串
+# hm_getstring
+
+
+
+# 命令行输入栏位置设置
+# hm_setcommandposition top
+# hm_setcommandposition bottom
+
+
+
+# 视角设置
+# *view leftside
+# *view rightside
+# *view rear
+# *view iso1
